6aaa895045625246f5024655dd0fd9e938724edf
T3CHTAYL0R <techtaylor92@outlook.com>
2024-02-12 15:40:15 -0500

    Added loop that takes user guesses and checks to see if it is correct, if it is not correct it tells the user if the number is higher or lower

--- main.py
+++ main.py
@@ -12,4 +12,26 @@ else:
     print("Please type a number")
     quit()        
 
-random_number = random.randrange(-1, 11)
+random_number = random.randint(0, top_of_range)
+guesses = 0 
+
+while True:
+    guesses += 1
+    user_guess = input("Make A Guess. ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print("Please type a number.")
+        continue
+
+    if user_guess == random_number:
+        print("Correct")
+        break
+    elif user_guess > random_number:
+        print("Lower!")
+    else:
+        print("Higher!")
+print("You got it correct in", guesses, "Guesses!")        
+
+            
+
